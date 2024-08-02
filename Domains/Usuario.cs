d94481da2cf162c06597a7d7669a4bb81d33b5d7
Walace <gbwalace@gmail.com>
2024-08-02 12:21:56 -0300

    Refatoração do projeto para organizar melhor os arquivos de classe

--- Domains/Usuario.cs
+++ Domains/Usuario.cs
@@ -1,4 +1,4 @@
-﻿namespace ClubeDoLivro
+﻿namespace ClubeDoLivro.Domains
 {
     public class Usuario
     {
--- Domains/Versao.cs
+++ Domains/Versao.cs
@@ -1,4 +1,4 @@
-﻿namespace ClubeDoLivro
+﻿namespace ClubeDoLivro.Domains
 {
     public enum Versao
     {
